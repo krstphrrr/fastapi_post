cf12b24bdf00ebe380ce5617c25480ca939ceb50
Kris! <42949874+krstphrrr@users.noreply.github.com>
2020-09-15 11:07:48 -0600

    api receives df and uses scripts successfully to QC then ingest

--- main.py
+++ main.py
@@ -7,21 +7,41 @@ from typing import List
 
 from utils import crud, models, schemas
 from utils.database import SessionLocal, engine
-
+# to actually ingest
+from tall_tables.talltables_handler import ingesterv2
+# to create tables and check for existence
+from dima.tabletools import table_create, tablecheck
+# aero test run:
+from aero.aero import model_run_updater
 from pandas.io.json import json_normalize
 
 
 import pandas as pd
 
 app = FastAPI()
+Test = schemas.TestModel
 
 @app.get("/")
 def root():
     return {"message": "fastapi for post requests up!"}
 
-@app.post("/api/v0/add-new")
-def respond(data:List[Test]=None):
+@app.post("/met")
+def post_met():
+    return {"message":"met data uploader"}
+
+@app.post("/nri")
+def post_nri():
+    return {"message":"nri data uploader"}
+
+@app.post("/tall")
+def post_tall(data:List[Test]=None):
 
     m = pd.DataFrame(jsonable_encoder(data))
 
     print(m)
+# aero test
+@app.post("/aero")
+def post_aero(data:List[Test]=None):
+    m = pd.DataFrame(jsonable_encoder(data))
+    model_run_updater(m)
+    # print(m)
--- post_api_debug.py
+++ post_api_debug.py
@@ -3,9 +3,10 @@ import requests
 import pickle
 import base64
 
-txt_path = r"C:\Users\kbon3efont\Desktop\201143010401R1_flux.txt"
+txt_path = r"C:\Users\kbonefont\Desktop\201143010401R1_flux.txt"
 df = pd.read_table(txt_path)
 
 df_json = df.to_json(orient='records')
-
-requests.post('http://localhost:8000/api/v0/add-new', data=df_json)
+df_json
+requests.post('http://localhost:5009/aero', data=df_json)
+requests.post('http://localhost:8000/aero', data=df_json)
